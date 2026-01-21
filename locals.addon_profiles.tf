5d21a92e382892bdd55ed0ecd7cea074fd8caf20
James Wilkinson <JWilkinson@mayerbrown.com>
2026-01-21 10:15:34 +0000

    fix: correct variable names for key vault secrets provider configuration

--- locals.addon_profiles.tf
+++ locals.addon_profiles.tf
@@ -33,8 +33,8 @@ locals {
       azureKeyvaultSecretsProvider = {
         enabled = true
         config = tomap({
-          enableSecretRotation = var.addon_profile_key_vault_secrets_provider.secret_rotation_enabled
-          rotationPollInterval = var.addon_profile_key_vault_secrets_provider.secret_rotation_interval
+          enableSecretRotation = var.addon_profile_key_vault_secrets_provider.enable_secret_rotation
+          rotationPollInterval = var.addon_profile_key_vault_secrets_provider.rotation_poll_interval
         })
       }
     } : null,
